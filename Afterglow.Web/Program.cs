1d4a48c228832dc81ca49ca2dfa477c8e0d0f6c9
spazzarama <spazzarama@gmail.com>
2012-07-31 19:15:48 +1000

    Added empty Afterglow.Web project

---
+++ Afterglow.Web/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Afterglow.Web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
